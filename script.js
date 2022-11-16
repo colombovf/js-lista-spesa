431ffe7da6864aeb860aea41617e12dda15c6c2d
colombovf <colombovf@gmail.com>
2022-11-16 15:17:42 +0100

    Ho scordato di committare

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 /* Data una lista della spesa già compilata nel vostro codice, stampare in console gli elementi della lista individualmente,
  prima con un ciclo for, poi con un ciclo while. Quando le stampe in console sono corrette, stampare gli elementi in un elenco 
  puntato in HTML, utilizzando uno dei due cicli scritti al punto precedente.*/
- let lista_spesa = ["prosciutto", "acqua", "coca-cola", "yogurt", "fiocchi d'avena"];
+
+let lista_spesa = ["Prosciutto", "Acqua", "Coca-cola", "Yogurt", "Fiocchi d'avena"];
+let i = 0;
+let collezione = document.getElementsByClassName("demo");
+
+
+
+   while (i < lista_spesa.length) { 
+      collezione[i].innerHTML = lista_spesa[i];
+      console.log(lista_spesa[i]);
+      i++;
+   }
+
+
+for (let j = i; j < lista_spesa.length + i; j ++) {
+    collezione[j].innerHTML = lista_spesa[j-i];
+    console.log(lista_spesa[j]);
+ }
+
+
